e47b398af8bbab87c543758ddd359f11b793b2dc
ChanHyukLeee <kkmi1127@gmail.com>
2021-12-27 22:03:39 +0900

    21.12.27

--- boj1406.cpp
+++ boj1406.cpp
@@ -1,19 +1,24 @@
 // 한줄로 된 간단한 에디터 구현 영어 소문자만
 
-
 #include <bits/stdc++.h>
 using namespace std;
-int abc[27], abc1[27];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  string str1, act1. act2;
-  int n;
-  cin >> str1;
+  list<int> L;
+  string str, act1, act2;
+  int n, cursor = 0;
+  cin >> str;
+  for (auto c :str) L.push_back(c); // 연결리스트에 저장
   cin >> n;
+  cursor = 
   for (int i = 0; i<n; i++){
-    cin >> act1 >> act2;
+    cin >> act1;
+    if(act1 = 'P'){
+      cin >> act2;
+      L.insert
+    }
     
   }
 
